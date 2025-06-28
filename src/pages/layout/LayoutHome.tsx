dd2549adaf30eb842f538e7d883daf56d76d4285
Asaed Reyes Medina <asaedreyesmedina@gmail.com>
2025-06-28 00:46:01 -0600

    [REFactor] Simplifica la lógica de clases en LayoutHome
    Se optimizan las clases de estilo en el componente LayoutHome, eliminando el uso de clsx y mejorando la legibilidad del código. Se ajustan las transiciones y la estructura de los contenedores para una mejor responsividad y experiencia de usuario.

--- src/pages/layout/LayoutHome.tsx
+++ src/pages/layout/LayoutHome.tsx
@@ -1,22 +1,21 @@
 import { LAYOUT_HOME_CONSTS } from "./../constants/LayoutHomeConsts"
 import SpecializationCard from "./../components/SpecializationCard"
-import clsx from "clsx"
 import useLayoutHome from "../hooks/useLayoutHome"
 
 
 const LayoutHome = () => {
   const { isBackendHidden, isFrontendHidden, specialization, handleSpecialization, hovered, setHovered } = useLayoutHome()
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-12 md:grid-cols-12 lg:grid-cols-12 xl:grid-cols-12 2xl:grid-cols-12 p-4 h-screen bg-black">
+    <div className="p-4 w-full h-screen bg-black flex flex-col md:flex-row">
       {/* FRONTEND WRAPPER */}
       <div
-        className={clsx(
-          isFrontendHidden && "hidden",
-          specialization === "backend" ? "opacity-0 scale-95 pointer-events-none" : "opacity-100 scale-100",
-          specialization === "frontend" ? "col-span-12" : "col-span-6",
-          "w-full h-full relative transition-all duration-500 ease-in-out",
-          hovered === "backend" && "blur-sm"
-        )}
+        className={`
+          ${isFrontendHidden ? " -translate-y-0 " : "w-1/2"}
+          ${specialization === "backend" ? "" : ""}
+          h-full relative transition-all duration-400 ease-in-out
+          ${hovered === "backend" ? "blur-sm" : ""}
+          ${specialization === "frontend" ? "w-full" : "w-1/2"}
+        `}
       >
         {hovered === "frontend" || specialization === "frontend" && (
           <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-blue-500 to-pink-500 blur-sm z-0" />
@@ -27,13 +26,13 @@ const LayoutHome = () => {
 
       {/* BACKEND WRAPPER */}
       <div
-        className={clsx(
-          isBackendHidden && "hidden",
-          specialization === "frontend" ? "opacity-0 scale-95 pointer-events-none" : "opacity-100 scale-100",
-          specialization === "backend" ? "col-span-12" : "col-span-6",
-          "w-full h-full relative transition-all duration-500 ease-in-out",
-          hovered === "frontend" && "blur-sm"
-        )}
+        className={`
+          ${isBackendHidden ? " -translate-y-0" : "w-1/2"}
+          ${specialization === "frontend" ? "" : ""}
+          h-full relative transition-all duration-400 ease-in-out
+          ${hovered === "frontend" ? "blur-sm" : ""}
+          ${specialization === "backend" ? "w-full" : "w-1/2"}
+        `}
       >
         <SpecializationCard title={LAYOUT_HOME_CONSTS.backend.title} description={LAYOUT_HOME_CONSTS.backend.description}
           tecnologies="backend" handleSpecialization={handleSpecialization} setHovered={setHovered} specialization={specialization} hovered={hovered} />
