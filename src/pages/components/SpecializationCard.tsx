46089b3a6a862beead7d04c9eff2e353cf8fb14b
Asaed Reyes Medina <asaedreyesmedina@gmail.com>
2025-06-27 21:41:53 -0600

    [Create] SpecializationCard
    Se crea nuevo componente para ser mas reutilizable y escable

--- src/pages/components/SpecializationCard.tsx
+++ src/pages/components/SpecializationCard.tsx
@@ -1,21 +1,23 @@
 import { Card } from "@/components/ui/card"
+import { cardStyles } from "./../styles/cardStyles.ts"
 
 export interface SpecializationCardProps {
     title: string
     description: string
+    tecnologies: "frontend" | "backend"
     specialization: "frontend" | "backend" | null
     handleSpecialization: (specialization: "frontend" | "backend") => void
     hovered: "frontend" | "backend" | null
     setHovered: (hovered: "frontend" | "backend" | null) => void
 }
 
-const SpecializationCard = ({ title, description, specialization, handleSpecialization, hovered, setHovered }: SpecializationCardProps) => {
+const SpecializationCard = ({ title, description, tecnologies = "frontend", specialization, handleSpecialization, hovered, setHovered }: SpecializationCardProps) => {
     return (
-        <Card className="relative z-10 w-full h-full flex flex-col items-center justify-center text-neutral-400 hover:text-white">
+        <Card className={`${cardStyles[tecnologies].card}`}>
             {
                 specialization === "frontend" ? null
                     : (
-                        <h3 className="scroll-m-20 xl:text-2xl lg:text-xl md:text-lg sm:text-base font-semibold tracking-tight">
+                        <h3 className={`${cardStyles[tecnologies].subtitle}`}>
                             {description}
                         </h3>
                     )
@@ -24,17 +26,15 @@ const SpecializationCard = ({ title, description, specialization, handleSpeciali
                 onClick={() => handleSpecialization("frontend")}
                 onMouseEnter={() => setHovered("frontend")}
                 onMouseLeave={() => setHovered(null)}
-                className={`
-        scroll-m-20 border-b-2 md:border-none text-center xl:text-7xl 
-        lg:text-6xl md:text-5xl sm:text-3xl text-3xl font-extrabold 
-        tracking-tight text-balance cursor-pointer hover:bg-gradient-to-r
-      hover:from-blue-500 hover:to-pink-500 hover:text-transparent hover:bg-clip-text
-      ${hovered === "frontend" || specialization === "frontend" ? "bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text" : ""}
+                className={` 
+                    ${cardStyles[tecnologies].title}
+                    ${hovered === "frontend" || specialization === "frontend" ? cardStyles[tecnologies].titleHover : null}
+                    ${hovered === "backend" || specialization === "backend" ? null : null}
         `}
             >
                 {title}
             </h1>
-        </Card>
+        </Card >
     )
 }
 
--- src/pages/styles/cardStyles.ts
+++ src/pages/styles/cardStyles.ts
@@ -1,14 +1,27 @@
-export const cardStyles = {
+export interface CardStylesProps {
+  frontend: cardContentProps;
+  backend: cardContentProps;
+}
+export interface cardContentProps {
+  card: string;
+  title: string;
+  titleHover?: string;
+  subtitle?: string;
+}
+export const cardStyles: CardStylesProps = {
   frontend: {
-    card: "",
-    title:
-      "",
+    card: "relative z-10 w-full h-full flex flex-col items-center justify-center text-neutral-400 hover:text-white",
+    title: `scroll-m-20 border-b-2 md:border-none  text-center xl:text-7xl lg:text-6xl md:text-5xl
+    sm:text-3xl text-3xl font-extrabold tracking-tight text-balance cursor-pointer hover: bg-gradient-to-rhover:from-blue-500 
+    hover:to-pink-500 hover:text-transparent hover:bg-clip-text`,
+    titleHover: `bg-gradient-to-r from-blue-500 to-pink-500 text-transparent bg-clip-text`,
     subtitle:
-      "",
+      "scroll-m-20 xl:text-2xl lg:text-xl md:text-lg sm:text-base font-semibold tracking-tight",
   },
   backend: {
-    card: "",
-    title:
-      "",
+    card: ``,
+    title: ``,
+    titleHover: ``,
+    subtitle: ``
   },
 };
